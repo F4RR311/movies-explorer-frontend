95abfcda4b5f7b5ac908d6713bd39860f7a857be
F4RR311 <valery.leo2016@yandex.ru>
2022-08-28 12:00:14 +0300

    fix movies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'http://localhost:3001';
-const MOVIES_URL = 'http://localhost:3000';
+const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 const SHORTMOVIES_DURATION = 40;
 const DEVICE_PARAMS = {
     desktop: {
